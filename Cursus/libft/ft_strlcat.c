8383da76a5779c8cc8dd7e95734aa75f1fa24347
BobMazuelo <bobsnokutc@gmail.com>
2022-01-25 10:57:12 +0100

    New files

--- Cursus/libft/ft_strlcat.c
+++ Cursus/libft/ft_strlcat.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#define FT_STRNLEN(dst, size) (sizeof(dst) / sizeof(*(dst))) - size
+#define FT_STRNLEN(dst, size) (sizeof(dst) / sizeof(*(dst))) - 1 - size
 
 size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {

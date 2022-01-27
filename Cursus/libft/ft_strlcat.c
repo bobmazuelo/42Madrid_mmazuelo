0f447f900caac331ee6677e320d9d6529efbe861
BobMazuelo <bobsnokutc@gmail.com>
2022-01-27 21:38:57 +0100

    Update files

--- Cursus/libft/ft_strlcat.c
+++ Cursus/libft/ft_strlcat.c
@@ -12,65 +12,19 @@
 
 #include "libft.h"
 
-
-#include <stdio.h>
-#include <string.h>
-
-/*
-size_t  ft_strlcat(char *restrict dst, const char *restrict src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    const size_t srclen = strlen(src);
-    const size_t dstlen = strnlen(dst, size);
+    const size_t srclen = ft_strlen(src);
+    const size_t dstlen = ft_strnlen(dst, size);
 
-    if (dstlen == size) 
-        return size + srclen;
-    if (srclen < size - dstlen) {
-        memcpy(dst + dstlen, src, srclen + 1);
-    } else {
-        memcpy(dst + dstlen, src, size - 1);
+    if (dstlen == size)
+        return (size + srclen);
+    if (srclen < size - dstlen)
+        ft_memcpy(dst + dstlen, src, srclen + 1);
+    else
+    {
+        ft_memcpy(dst + dstlen, src, size - 1);
         dst[dstlen + size - 1] = '\0';
     }
     return (dstlen + srclen);
-}
-*/
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	len;
-	size_t	i;
-
-	len = 0;
-	i = 0;
-	while (dst[i] && i < dstsize)
-		i++;
-	len = i;
-	while (src[i - len] && i + 1 < dstsize)
-	{
-		dst[i] = src[i - len];
-		i++;
-	}
-	if (len < dstsize)
-		dst[i] = '\0';
-	return (len + strlen(src));
-}
-
-int main()
-{
-    char first[] = "This is ";
-    char last[] = "a potentially long string";
-    int r;
-    int size = 16;
-    char buffer[size];
-
-    strcpy(buffer,first);
-    r = ft_strlcat(buffer,last,size);
-
-    puts(buffer);
-    printf("Value returned: %d\n",r);
-    if( r > size )
-        puts("String truncated");
-    else
-        puts("String was fully copied");
-
-    return(0);
 }

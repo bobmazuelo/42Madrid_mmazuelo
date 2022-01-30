3386f26eac7846cb55cca695b0317611e499ba4f
Miguel Mazuelo alvarez <mmazuelo@c1r4s2.42madrid.com>
2022-01-30 14:00:25 +0100

    Update norminette in all files

--- Cursus/libft/ft_memchr.c
+++ Cursus/libft/ft_memchr.c
@@ -6,11 +6,23 @@
 /*   By: mmazuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 21:06:33 by mmazuelo          #+#    #+#             */
-/*   Updated: 2022/01/30 13:12:24 by mmazuelo         ###   ########.fr       */
+/*   Updated: 2022/01/30 14:00:03 by mmazuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	return (s);
+	size_t		i;
+	const char	*str;
+
+	i = 0;
+	str = s;
+	while (*str != '\0' && i < n)
+	{
+		if (*str == c)
+			return ((void *)(str));
+		str++;
+		i++;
+	}
+	return (0);
 }

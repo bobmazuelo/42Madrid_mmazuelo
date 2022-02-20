ba97bdefe506055c4a340259676cc8a3e4b9b645
Kodi2023 <bobsnokutc@gmail.com>
2022-02-21 00:13:02 +0100

    Update

--- Cursus/ft_printf/ft_printf.c
+++ Cursus/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mmazuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 09:02:43 by mmazuelo          #+#    #+#             */
-/*   Updated: 2022/02/21 00:10:45 by mmazuelo         ###   ########.fr       */
+/*   Updated: 2022/02/21 00:12:20 by mmazuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_printf(const char *ftm, ...)
 {
-	int	done;
 	va_list	args;
+	int	done;
 	char	*str_print;
 	
 	va_start(args, ftm);

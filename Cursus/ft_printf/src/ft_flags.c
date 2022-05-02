de4cb42ffb331c47dbc0e3ea0524594b2dddb2b0
Miguel Mazuelo alvarez <mmazuelo@c3r12s3.42madrid.com>
2022-05-02 19:49:13 +0200

    ft_pprintf PUTA ZORRA DE MIERDA ME CAGO EN DIOS Y E EL PUTO GITHUB

--- Cursus/ft_printf/src/ft_flags.c
+++ Cursus/ft_printf/src/ft_flags.c
@@ -6,7 +6,7 @@
 /*   By: mmazuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 01:52:39 by mmazuelo          #+#    #+#             */
-/*   Updated: 2022/05/02 18:27:39 by mmazuelo         ###   ########.fr       */
+/*   Updated: 2022/05/02 19:40:01 by mmazuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,23 @@ void	ft_flags(char *str, va_list fmt)
 	if (*str == '%')
 		ft_putchar('%');
 	else if (*str == 's')
-		ft_string(fmt);
+		ft_str(fmt);
 	else if (*str == 'c')
 		ft_char(fmt);
 	else if (*str == 'p')
-		ft_pointer(fmt);
+		ft_ptr(fmt);
 	else if (*str == 'x' || 'X')
-		ft_hexa(fmt, *str);
+		ft_hex(fmt, *str);
 	else if (*str == 'i' || *str == 'd')
 		ft_int(fmt);
 	else if (*str == 'u')
 		ft_unsigned(fmt);
 	else if (*str == 'e')
-		ft_scientific(fmt);
+		ft_scf(fmt);
 	else if (*str == 'f')
 		ft_float(fmt);
 	else if (*str == 'o')
-		ft_octal(fmt);
+		ft_oct(fmt);
 	else
 		return (NULL);
 }
--- Cursus/ft_printf/src/ft_putnbrbase.c
+++ Cursus/ft_printf/src/ft_putnbrbase.c
@@ -1,13 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sfc.c                                           :+:      :+:    :+:   */
+/*   ft_putnbrbase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmazuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/02 19:38:37 by mmazuelo          #+#    #+#             */
-/*   Updated: 2022/05/02 19:38:38 by mmazuelo         ###   ########.fr       */
+/*   Created: 2022/05/02 19:41:25 by mmazuelo          #+#    #+#             */
+/*   Updated: 2022/05/02 19:41:59 by mmazuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+void	ft_print_num(long long num, int base_l, char *base)
+{
+	if (num >= base_l)
+		ft_print_num(num / base_l, base_l, base);
+	write(1, &base[num % base_l], 1);
+}

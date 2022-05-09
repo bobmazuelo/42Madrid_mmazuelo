bd9ba3ff30365a855a94b1ab05482ae6f1715704
Miguel Mazuelo alvarez <mmazuelo@c1r10s3.42madrid.com>
2022-05-09 20:43:26 +0200

    Se vienen cositas

---
+++ Cursus/ft_printf/src/ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmazuelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 20:02:37 by mmazuelo          #+#    #+#             */
+/*   Updated: 2022/05/08 22:03:46 by mmazuelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <limits.h>
+# include <unistd.h>
+
+int		ft_printf(const char *fmt, ...);
+int		ft_vprintf(const char *fmt, va_list args);
+int		ft_char(int c);
+int		ft_hex(unsigned int nb, char str);
+int		ft_int(int nb);
+int		ft_oct(unsigned int nb);
+int		ft_ptr(void *ptr);
+int		ft_str(char *str);
+int		ft_unsigned(unsigned int nb);
+
+int		ft_print_num(long long num, int base_l, char *base, int *done);
+
+#endif

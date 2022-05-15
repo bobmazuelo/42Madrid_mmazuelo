d9b9b3084b244085430273b9b959ed109fc04161
Kodi2023 <bobsnokutc@gmail.com>
2022-05-15 10:01:17 +0200

    GNL

--- Cursus/gnl/main.c
+++ Cursus/gnl/main.c
@@ -1,26 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmazuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/09 19:30:46 by mmazuelo          #+#    #+#             */
-/*   Updated: 2022/05/15 09:57:25 by mmazuelo         ###   ########.fr       */
+/*   Created: 2022/05/15 09:52:39 by mmazuelo          #+#    #+#             */
+/*   Updated: 2022/05/15 09:53:38 by mmazuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
+#include "get_next_line.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-char	*get_next_line(int fd);
-size_t	ft_strclen(const char *str, int c);
-char	*ft_join(const char *s1, const char *s2);
-char	*ft_strdup(const char *str);
-char	*ft_strchr(const char *str, int c);
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*str;
 
-#endif
+	fd = open(argv[1], O_RDONLY);
+	str = get_next_line(fd);
+	while (*str != '\0')
+	{
+		printf("|%s|\n", str);
+		free(str);
+		str = get_next_line(fd);
+	}
+	return (0);
+}
